89ab7d20cd0883a96707df293bf9561144e2a82d
William Spencer <wjbs@uchicago.edu>
2026-01-26 16:03:10 -0600

    Add SummedElement class to reason about elements that are summed over for a Summable type

--- theories/TensorCore/SummableWF.v
+++ theories/TensorCore/SummableWF.v
@@ -15,7 +15,7 @@ Class WFSummable (A : Type) `{Summable A} := mk_WF_sum {
   mk_WF_sum (fin_elements_NoDup n).
 
 
-#[export] Program Instance SummableWF_vec `{WFSummable A} {n} : 
+#[export] Program Instance SummableWF_vec `{WFSummable A} {n} :
   WFSummable (vec A n) :=
   mk_WF_sum (vec_elements_NoDup _ n NoDup_sum_elements).
 
@@ -59,3 +59,70 @@ Next Obligation.
   intros ? ?%elem_of_list_fmap ?%elem_of_list_fmap.
   firstorder congruence.
 Qed.
+
+Class SummedElement `{Summable A} (a : A) : Prop := {
+  element_summed : a ∈ sum_elements
+}.
+
+#[export] Program Instance SummedElement_fin {n} (i : fin n) :
+  SummedElement i.
+Next Obligation.
+  intros.
+  apply elem_of_list_In, fin_elements_in.
+Qed.
+
+#[export] Program Instance SummedElement_bool (b : bool) :
+  SummedElement b.
+Next Obligation.
+  intros []; cbn; compute_done.
+Qed.
+
+(* FIXME: Move *)
+Lemma vec_to_list_to_list {A n} (v : vec A n) :
+  vec_to_list v = Vector.to_list v.
+Proof.
+  induction v; cbn; f_equal; easy.
+Qed.
+
+#[export] Program Instance SummedElement_vec `{Summable A}
+  {n} (v : vec A n) (Hv : forall a, a ∈ vec_to_list v -> SummedElement a) : 
+  SummedElement v.
+Next Obligation.
+  intros A SA n v Hv.
+  apply elem_of_list_In, vec_elements_in.
+  rewrite Vector.to_list_Forall, Forall_forall.
+  intros a Ha.
+  rewrite <- vec_to_list_to_list in Ha.
+  now apply elem_of_list_In, Hv.
+Qed.
+
+#[export] Program Instance SummedElement_sum_l `{Summable A, Summable B}
+  (a : A) `{Ha : !SummedElement a} :
+  SummedElement (@inl A B a).
+Next Obligation.
+  intros A SA B SB a [Ha].
+  cbn.
+  apply elem_of_app; left.
+  now apply elem_of_list_fmap_1.
+Qed.
+
+#[export] Program Instance SummedElement_sum_r `{Summable A, Summable B}
+  (b : B) `{Ha : !SummedElement b} :
+  SummedElement (@inr A B b).
+Next Obligation.
+  intros A SA B SB b [Hb].
+  cbn.
+  apply elem_of_app; right.
+  now apply elem_of_list_fmap_1.
+Qed.
+
+#[export] Program Instance SummedElement_prod `{Summable A, Summable B}
+  (ab : A * B) `{Ha : !SummedElement ab.1, Hb : !SummedElement ab.2} :
+  SummedElement ab.
+Next Obligation.
+  intros A SA B SB a [Ha] [Hb].
+  cbn.
+  rewrite <- list_cprod_list_prod.
+  now apply elem_of_list_cprod.
+Qed.
+
