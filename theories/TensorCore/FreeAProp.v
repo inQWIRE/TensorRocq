8c72ce340278c00c59dd72358aafd9b1ad90034b
William Spencer <wjbs@uchicago.edu>
2026-03-09 21:02:27 -0500

    Add to chyp Frobenius example

--- theories/TensorCore/FreeAProp.v
+++ theories/TensorCore/FreeAProp.v
@@ -857,5 +857,135 @@ Proof.
   smcat.
 Qed.
 
+Notation id := (Aid 1).
+
+Definition cup : AProp _ _ _ := u ;' n.
+Definition cap : AProp _ _ _ := m ;' v.
+
+Lemma cap_assoc : m * Aid 1 ;' cap == Aid 1 * m ;' cap.
+Proof.
+  unfold cap.
+  srw assoc.
+  smcat.
+Qed.
+
+
+Lemma cup_assoc : cup ;' n * Aid 1 == cup ;' Aid 1 * n.
+Proof.
+  unfold cup.
+  srw coassoc.
+  smcat.
+Qed.
+
+Lemma yankL : cup * Aid 1 ;' Aid 1 * cap == Aid 1.
+Proof.
+  unfold cup, cap.
+  srw frobL.
+  srw unitL.
+  srw counitR.
+  smcat.
+Qed.
+
+Lemma yankR : Aid 1 * cup ;' cap * Aid 1 == Aid 1.
+Proof.
+  unfold cup, cap.
+  srw frobR.
+  srw unitR.
+  srw counitL.
+  smcat.
+Qed.
+
+Lemma m_cupL : cup * Aid 1 ;' Aid 1 * m == n.
+Proof.
+  unfold cup.
+  srw frob.
+  srw unitL.
+  smcat.
+Qed.
+
+Lemma m_cupR : Aid 1 * cup ;' m * Aid 1 == n.
+Proof.
+  unfold cup.
+  srw <- frob.
+  srw unitR.
+  smcat.
+Qed.
+
+Lemma n_capL : Aid 1 * n ;' cap * Aid 1 == m.
+Proof.
+  unfold cap.
+  srw <- frob.
+  srw counitL.
+  smcat.
+Qed.
+
+Lemma n_capR : n * id ;' id * cap == m.
+Proof.
+  unfold cap.
+  srw frob.
+  srw counitR.
+  smcat.
+Qed.
+
+Definition m2 : AProp _ _ _ := id * Aswap 1 1 * id ;' m * m.
+Definition u2 : AProp _ _ _ := u * u.
+
+Definition n2 : AProp _ _ _ := n * n ;' id * Aswap 1 1 * id.
+Definition v2 : AProp _ _ _ := v * v.
+
+Lemma assoc2 : m2 * Aid 2 ;' m2 ==
+  Aid 2 * m2 ;' m2.
+Proof.
+  unfold m2.
+  srw assoc.
+  srw assoc.
+  smcat.
+Qed.
+
+Lemma unitL2 : u2 * Aid 2 ;' m2 == Aid 2.
+Proof.
+  srw unitL.
+  srw unitL.
+  smcat.
+Qed.
+
+Lemma unitR2 : Aid 2 * u2 ;' m2 == Aid 2.
+Proof.
+  srw unitR.
+  srw unitR.
+  smcat.
+Qed.
+
+Lemma coassoc2 : n2 ;' n2 * Aid 2 ==
+  n2 ;' Aid 2 * n2.
+Proof.
+  unfold n2.
+  srw coassoc.
+  srw coassoc.
+  smcat.
+Qed.
+
+Lemma counitL2 : n2 ;' v2 * Aid 2 == Aid 2.
+Proof.
+  srw counitL.
+  srw counitL.
+  smcat.
+Qed.
+
+Lemma counitR2 : n2 ;' Aid 2 * v2 == Aid 2.
+Proof.
+  srw counitR.
+  srw counitR.
+  smcat.
+Qed.
+
+Lemma frob2 : n2 * Aid 2 ;' Aid 2 * m2 == Aid 2 * n2 ;' m2 * Aid 2.
+Proof.
+  srw frob.
+  srw frob.
+  smcat.
+Qed.
+
+
 End FrobExample.
 
