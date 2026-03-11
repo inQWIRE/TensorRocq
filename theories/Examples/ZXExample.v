5aa594811aca4ce96b98aba2499e94827bcc8561
William Spencer <wjbs@uchicago.edu>
2026-03-11 15:31:29 -0500

    Fix ZX example

--- theories/Examples/ZXExample.v
+++ theories/Examples/ZXExample.v
@@ -1,7 +1,16 @@
 Require Export FreeAProp.
-Require Import Qcanon.
+Require Import QArith Qcanon.
 
 
+#[export] Instance Qc_inhabited : Inhabited Qc := populate 0%Qc.
+
+Declare Custom Entry print_qc.
+
+Notation "q" := (q) (only printing, q constr, in custom print_qc at level 0).
+
+Notation "q " := (Qcmake q%Q _) (only printing, q constr, in custom print_qc at level 0).
+
+Notation " q" := (Q2Qc q%Q) (only printing, q constr, in custom print_qc at level 0).
 
 
 Local Open Scope aprop_scope.
@@ -11,8 +20,13 @@ Notation "x === y" :=
     {n & {m & (AProp (fin 2) n m * AProp (fin 2) n m)%type}})
   (at level 70).
 
-Notation Z n m q := (Agen ((0%fin :> fin 2, q%Qc)) n m).
-Notation X n m q := (Agen ((1%fin :> fin 2, q%Qc)) n m).
+Notation Z n m q := (Agen (0%fin :> fin 2, q%Qc) n m) (only parsing).
+Notation X n m q := (Agen (1%fin :> fin 2, q%Qc) n m) (only parsing).
+
+Notation "'Z' n m q" := (Agen (0%fin, q) n m) (only printing, 
+  q custom print_qc, at level 10).
+Notation "'X' n m q" := (Agen (1%fin, q) n m) (only printing, 
+  q custom print_qc, at level 10).
 
 Inductive ZXEq : forall {n m}, relation (AProp (fin 2 * Qc) n m) :=
   | ZXEq_fuseZ n m o q q' : 
@@ -27,9 +41,17 @@ Definition ZX : Signature bool := {|
 
 Notation "x == y" :=
   (x ≡ᵣ@{ZX} y)
-  (at level 70, y custom aprop).
+  (at level 70).
 
 Lemma fuseZ n m o q q' : Z n m q ;' Z m o q' == Z n o (q + q').
 Proof. apply rules_hold; constructor. Qed.
 
+
+
+Example test_fuse : Z 1 2 0 * X 2 1 0 ;' Z 2 1 1 * id == 
+  Z 1 1 1 * X 2 1 0.
+Proof.
+  srw_lhs (fuseZ 1 2 1 0 1).
+  smcat.
+Qed.
 
