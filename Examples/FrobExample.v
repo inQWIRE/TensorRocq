929dcac4b23040f9b2fd6049366c58b750c877a3
William Spencer <wjbs@uchicago.edu>
2026-04-01 15:54:21 -0500

    Document FrobExample

--- Examples/FrobExample.v
+++ Examples/FrobExample.v
@@ -1,18 +1,44 @@
 From TensorRocq Require Export FreeAProp.
 
+(** To support reasoning about abstract theories in the style of CHyP,
+  we use a system that involves some boilerplate code. To demonstrate
+  how this is done, we provide the following direct translation of the 
+  Frobenius algebra example from CHyP
+  (frobenius.chyp)[https://github.com/akissinger/chyp/blob/master/examples/frobenius.chyp].
+  (The original comments are included inline, as appropriate.)
+  *)
+
+(** To more easily state the assumed relations of our theory, 
+  we define the following notation. Note that the [4] of [fin 4]
+  must be changed to the number of generators of the theory. *)
 Notation "x === y" :=
   (existT _ (existT _ (x%aprop, y%aprop)) :
     {n & {m & (AProp (fin 4) n m * AProp (fin 4) n m)%type}})
   (at level 70).
 
-Notation m := (Agen (0%fin :> fin 4) 2 1) (only parsing).
-Notation u := (Agen (1%fin :> fin 4) 0 1) (only parsing).
-Notation n := (Agen (2%fin :> fin 4) 1 2) (only parsing).
-Notation v := (Agen (3%fin :> fin 4) 1 0) (only parsing).
+(** We define notations for the generators of the theory, 
+  along with their dimensions, which allow us to state rules. *)
+(** As a technical note, we define these notations to be [only parsing]
+  and later define [only printing] versions to ensure generators are 
+  always printed with their names. *)
+Notation m (* multiplication *) := 
+  (Agen (0%fin :> fin 4) 2 1) (only parsing).
+Notation u (* unit *) := 
+  (Agen (1%fin :> fin 4) 0 1) (only parsing).
+Notation n (* comultiplication *) := 
+  (Agen (2%fin :> fin 4) 1 2) (only parsing).
+Notation v (* counit *) := 
+  (Agen (3%fin :> fin 4) 1 0) (only parsing).
 
+
+(** We define a [Signature] for the theory. 
+  [bool] can be replaced with any [Summable] type. *)
 Definition Frob : Signature bool := {|
+  (** We define the type of generators... *)
   gens := fin 4;
+  (** ... and the equivalence relation among them (in this case, equality) *)
   gens_equiv := eq;
+  (** Then, we give the list of rules of the theory *)
   rules := rules_of_rule_list [
 
 (* (m, u) forms a monoid *)
@@ -31,16 +57,21 @@ Definition Frob : Signature bool := {|
      ];
 |}.
 
-
+(** We define [only printing] notations for the constructors to 
+  ensure they are printed. *)
 Notation "'m'" := (@Agen _ 0%fin 2 1) (only printing).
 Notation "'u'" := (@Agen _ 1%fin 0 1) (only printing).
 Notation "'n'" := (@Agen _ 2%fin 1 2) (only printing).
 Notation "'v'" := (@Agen _ 3%fin 1 0) (only printing).
 
+(** We use the following notation for equality in the theory of [Frob], 
+  as defined above. *)
 Notation "x == y" :=
   (x ≡ᵣ@{Frob} y)
   (at level 70).
 
+(** We restate the rules of the theory (note that here they use the 
+  new [==] relation), and give them names. *)
 (* (m, u) forms a monoid *)
 Lemma assoc : m * id ;' m == id * m ;' m.
 Proof. apply rules_hold. repeat constructor. Qed.
@@ -67,20 +98,50 @@ If m and n were both commutative, one of these would imply the other, and either
 would imply "frob". However, for non-commutative Frobenius algebras, we need them
 both. *)
 
+
+(** Now, we can prove lemmas in the same way as CHyP *)
+Lemma frobL_chyp : n * id ;' id * m == m ;' n.
+Proof.
+  (** The original proof is as follows:
+  n * id ; id * m
+  = u * n * id ; m * m by -unitL
+  = u * id * id ; n * id * id ; id * m * id ; id * m by -frob
+  = u * m ; n * id ; id * m by assoc
+  = u * m ; id * n ; m * id by frob
+  = m ; n by unitL
+  *)
+  (** We can replicate this style directly using transitivity statements, 
+    our rewrite tactic [srw], and our SMC solver [smcat] *)
+  transitivity [[u * n * id ; m * m]]%aprop; 
+    [srw unitL; smcat|].
+  transitivity [[u * id * id ; n * id * id ; id * m * id ; id * m]]%aprop; 
+    [srw <- frob; smcat|].
+  transitivity [[u * m ; n * id ; id * m]]%aprop; 
+    [srw assoc; smcat|].
+  transitivity [[u * m ; id * n ; m * id]]%aprop; 
+    [srw frob; smcat|].
+  transitivity [[m ; n]]%aprop; 
+    [srw unitL; smcat|].
+  smcat.
+Qed.
+
+(** However, most of these explicit transitivity steps can be elided, 
+  as we can find the rewrites automatically.
+  This is the style we will prefer. *)
+
 Lemma frobL : n * id ;' id * m == m ;' n.
 Proof.
   transitivity (u * n * id ;' m * m)%aprop; 
   [srw unitL; smcat|].
-  (* u * n * [[ id ]];' m * m == m;' n *)
+  (* [[ u * n * id; m * m ]] == [[ m; n ]] *)
   srw <- frob.
-  (* u * [[ id 2 ]];' Aswap 2 1;' [[ id ]] * (n * [[ id ]];' [[ id ]] * m);' (Aswap 2 1;' m * [[ id ]];' sw) == m;' n *)
+  (* [[ u * id 2; Aswap 2 1; id * (n * id; id * m); (Aswap 2 1; m * id; sw) ]] == [[ m; n ]] *)
   srw assoc.
-  (* u * [[ id 2 ]];' n * [[ id 2 ]];' [[ id ]] * ([[ id ]] * m;' m) ==
-m;' n *)
+  (* [[ u * id 2; n * id 2; id * (id * m; m) ]] == [[ m; n ]] *)
   srw frob.
-  (* m * u;' sw;' ([[ id ]] * n;' m * [[ id ]]) == m;' n *)
+  (* [[ m * u; sw; (id * n; m * id) ]] == [[ m; n ]] *)
   srw unitL.
-  (* m;' (n;' sw);' sw == m;' n *)
+  (* [[ m; (n; sw); sw ]] == [[ m; n ]] *)
   smcat.
 Qed.
 
@@ -93,6 +154,9 @@ Proof.
   smcat.
 Qed.
 
+(** As with CHyP, we can define derived morphisms. 
+  This is the final element to translate CHyP files; 
+  we include the rest for reference. *)
 Definition cup : AProp _ _ _ := u ;' n.
 Definition cap : AProp _ _ _ := m ;' v.
 
